29609d4ee5922f1c90d4c59fad24f12c6d5f30e0
mircomania <mirco.rodriguez@hotmail.com>
2025-11-19 16:51:48 -0300

    9 commit foodie guru - section 7 1.0

--- src/components/exports/SectionLp7.jsx
+++ src/components/exports/SectionLp7.jsx
@@ -3,7 +3,33 @@ import styles from '../../styles/modules/sectionLp7.module.css';
 export const SectionLp7 = () => {
     return (
         <section className={styles.sectionContainer}>
-            <h2>section 7</h2>
+            <header className={styles.sectionTitulo}>
+                <h2 className="formula-bold">
+                    ¿POR QUÉ
+                    <br />
+                    FOODIE GURÚ?
+                </h2>
+
+                <p className="marker-text">
+                    &quot;PARA ROMPER
+                    <br />
+                    LAS REGLAS
+                    <br />
+                    PRIMERO HAY QUE
+                    <br />
+                    CONOCERLAS&quot;
+                </p>
+            </header>
+
+            <div className={styles.sectionContenido}>
+                <p className="alliance-text">
+                    En primer lugar, por la basta experiencia que desarrollamos durante todo el proyecto y nuestra forma humana de hacer las cosas -
+                    somos muy obsesivos con el resultado, duros con la puntualidad y la disciplina. Por eso nosotros piloteamos el proyecto con el
+                    estandarte de ser siempre la antítesis de aquel viejo protocolo de gritos, mentadas de madre y malos modales. Únicos en el mercado
+                    que reconocemos que el activo #1 de un restaurante es su GENTE y es por eso que nuestra metodología está enfocada en VENDER,
+                    CONECTAR y CRECER juntos y así todos ganemos.
+                </p>
+            </div>
         </section>
     );
 };
