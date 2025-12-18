4c08eb2105a9a680a80fdfa288ecf07fc222cc46
mircomania <mirco.rodriguez@hotmail.com>
2025-12-18 17:53:06 -0300

    Updates

--- assets/FaqPage-D2sXiaaU.js
+++ assets/FaqPage-D2sXiaaU.js
@@ -1,4 +1,4 @@
-import{j as e,H as r,l as c}from"./index-rgm8VOGa.js";import{T as t}from"./TitleSEO-DUveym5r.js";import{s,P as d}from"./ParseTextToJSX-DXBP1g24.js";import{B as l}from"./BotonNav-KyHqfCz5.js";const p=a=>({"@context":"https://schema.org","@type":"FAQPage",mainEntity:a.map(o=>({"@type":"Question",name:o.question,acceptedAnswer:{"@type":"Answer",text:o.answer}}))}),n=[{id:1,question:"¿Qué precio tienen sus servicios?",answer:`Nuestros servicios están organizados en programas y proyectos con precio fijo según el alcance. Contamos con:
+import{j as e,H as r,l as c}from"./index-x_f9mlnX.js";import{T as t}from"./TitleSEO-DeuYsmHc.js";import{s,P as d}from"./ParseTextToJSX-DE41-pmF.js";import{B as l}from"./BotonNav-CfP-71A3.js";const p=a=>({"@context":"https://schema.org","@type":"FAQPage",mainEntity:a.map(o=>({"@type":"Question",name:o.question,acceptedAnswer:{"@type":"Answer",text:o.answer}}))}),n=[{id:1,question:"¿Qué precio tienen sus servicios?",answer:`Nuestros servicios están organizados en programas y proyectos con precio fijo según el alcance. Contamos con:
 
 - Acompañamientos estratégicos de 3 meses
 
